b0dc8d245dd8a2d251b5deab278fbd79fbcd429f
kallakunta.saisindhu <kallakunta.saisindhu@gmail.com>
2025-12-29 08:12:56 +0000

    RED- Test case added for Scanning item should return its unit price

--- CheckoutKata.Tests/CheckoutTests.cs
+++ CheckoutKata.Tests/CheckoutTests.cs
@@ -9,5 +9,18 @@ namespace CheckoutKata.Tests
 
             Assert.That(checkout.GetTotalPrice(), Is.EqualTo(0));
         }
+
+        [Test]
+        public void Single_Item_Returns_Unit_Price()
+        {
+            var rules = new PricingRules(
+                new Dictionary<string, int> { ["A"] = 50 }
+            );
+
+            var checkout = new Checkout(rules);
+            checkout.Scan("A");
+
+            Assert.That(checkout.GetTotalPrice(), Is.EqualTo(50));
+        }
     }
 }
