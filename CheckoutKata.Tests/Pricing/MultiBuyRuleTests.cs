a371f6f90828be960c456f7d329c60aad68effbc
kallakunta.saisindhu <kallakunta.saisindhu@gmail.com>
2026-01-05 02:45:45 +0530

    Test: added multi-buy pricing rule tests

---
+++ CheckoutKata.Tests/Pricing/MultiBuyRuleTests.cs
@@ -0,0 +1,21 @@
+﻿using CheckoutKata.Pricing;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CheckoutKata.Tests.Pricing
+{
+    public class MultiBuyRuleTests
+    {
+        [Test]
+        public void Applies_Offer_When_Threshold_Met()
+        {
+            var rule = new MultiBuyRule("A", 3, 130, 50);
+            var items = new[] { "A", "A", "A" };
+
+            Assert.That(rule.Calculate(items), Is.EqualTo(130));
+        }
+    }
+}
