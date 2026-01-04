96c4d384bd071167b312b47165e20a2494f9434c
kallakunta.saisindhu <kallakunta.saisindhu@gmail.com>
2026-01-05 02:48:06 +0530

    Test: added buy one get one pricing rule tests

---
+++ CheckoutKata.Tests/Pricing/BuyOneGetOneRuleTests.cs
@@ -0,0 +1,21 @@
+﻿using CheckoutKata.Pricing;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CheckoutKata.Tests.Pricing
+{
+    public class BuyOneGetOneRuleTests
+    {
+        [Test]
+        public void Every_Second_Item_Is_Free()
+        {
+            var rule = new BuyOneGetOneRule("A", 50);
+            var items = new[] { "A", "A", "A" };
+
+            Assert.That(rule.Calculate(items), Is.EqualTo(100));
+        }
+    }
+}
