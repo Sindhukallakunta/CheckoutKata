f44959d7ac32cf91dadc6756220d9013d2f9bcb3
kallakunta.saisindhu <kallakunta.saisindhu@gmail.com>
2026-01-05 02:39:38 +0530

    Test: added unit price rule calculation tests

---
+++ CheckoutKata.Tests/Pricing/UnitPriceRuleTests.cs
@@ -0,0 +1,23 @@
+﻿using CheckoutKata.Pricing;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CheckoutKata.Tests.Pricing
+{
+    public class UnitPriceRuleTests
+    {
+        [Test]
+        public void Calculates_Total_Using_Unit_Price()
+        {
+            var rule = new UnitPriceRule("A", 50);
+            var items = new[] { "A", "A" };
+
+            var total = rule.Calculate(items);
+
+            Assert.That(total, Is.EqualTo(100));
+        }
+    }
+}
