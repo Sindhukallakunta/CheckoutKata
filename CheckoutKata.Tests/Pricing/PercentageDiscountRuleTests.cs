e630226e61552573041aeb26da615e92a45065b1
kallakunta.saisindhu <kallakunta.saisindhu@gmail.com>
2026-01-05 02:51:11 +0530

    test: added percentage discount pricing rule tests

---
+++ CheckoutKata.Tests/Pricing/PercentageDiscountRuleTests.cs
@@ -0,0 +1,21 @@
+﻿using CheckoutKata.Pricing;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CheckoutKata.Tests.Pricing
+{
+    public class PercentageDiscountRuleTests
+    {
+        [Test]
+        public void Applies_Percentage_Discount()
+        {
+            var rule = new PercentageDiscountRule("A", 100, 0.10m);
+            var items = new[] { "A", "A" };
+
+            Assert.That(rule.Calculate(items), Is.EqualTo(180));
+        }
+    }
+}
